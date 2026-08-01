5a10fc1af1df5d33a8081fdf9c95ddcd770144e7
zyrridian <zyrridian@users.noreply.github.com>
2026-08-01 07:13:51 +0000

    deploy: 70df1d2eeac4960301b26909c66ff815ba8a3d6d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1900267730"
+    serviceWorkerVersion: "1677400608"
   }
 });
